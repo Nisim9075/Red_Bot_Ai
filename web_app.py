c9bde1b5253ae672df136045800d21be0775f3b7
Nisim9075 <shonlazmi@gmail.com>
2025-03-05 22:03:01 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -35,5 +35,5 @@ def chat():
 if __name__ == "__main__":
     
     # שימוש במשתנה סביבה PORT (Render דורש את זה)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1000))
     app.run(debug=True, host="0.0.0.0", port=port)
